6f3631707e77575ac2ffee6328ddd32c6ee38dc6
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2016-08-02 21:07:47 -0700

    Modified brew packages, rmoved Atom

---
+++ vim/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# vim
+#
+# This installs vundle
+
+vundledir=~/.vim/bundle/Vundle.vim
+if [ ! -d $vundledir ]; then
+  echo "  Installing Vundle for you."
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+exit 0
