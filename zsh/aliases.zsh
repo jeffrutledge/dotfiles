119a216bbf3974e89531052aa3474b85e2c63a2d
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2017-03-07 15:34:48 -0800

    added case for mac osx

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -32,8 +32,36 @@ function mvcd() {
 # Alias
 ##############################################################
 
-# mathmatica REPL
-alias math='/Applications/Mathematica.app/Contents/MacOS/MathKernel'
+# Mac OSX Exclusive aliases
+if [[ "$(uname -s)" == "Darwin" ]]
+then
+  # MacVim
+  alias vim='mvim -v'
+  alias vi='mvim'
+
+  # Skim
+  alias skim='open -a Skim $1'
+
+  # mathmatica REPL
+  alias math='/Applications/Mathematica.app/Contents/MacOS/MathKernel'
+
+  # Personal
+  # Open and close notes
+  alias notes='hdiutil attach ~/gdrive/personal/notes.dmg && cd /Volumes/notes'
+  function qnotes() {
+    cd ~
+    local disk_name=$(hdiutil info | grep /Volumes/notes | cut -f1)
+    hdiutil detach $disk_name
+  }
+  # Create a new journal entry
+  function new_journal_entry() {
+    notes
+    echo '\n##'$(date +"%Y-%m-%d %H:%M")'\n' >> journal.md
+    vim -c 'startinsert' + journal.md
+    qnotes
+  }
+  alias nje=new_journal_entry
+fi
 
 # SSH Server
 alias sshRut='ssh jrutledge@65.78.142.195'
@@ -48,30 +76,6 @@ alias 70clang='clang++ -std=c++11 -pedantic -Wall -Wextra'
 alias py2='python2'
 alias py3='python3'
 
-# MacVim
-alias vim='mvim -v'
-alias vi='mvim'
-
-# Skim
-alias skim='open -a Skim $1'
-
 # Murmur
 alias start_murmur='~/Desktop/murmur/murmurd'
 alias kill_murmur='killall -9 murmurd'
-
-# Personal
-# Open and close notes
-alias notes='hdiutil attach ~/gdrive/personal/notes.dmg && cd /Volumes/notes'
-function qnotes() {
-  cd ~
-  local disk_name=$(hdiutil info | grep /Volumes/notes | cut -f1)
-  hdiutil detach $disk_name
-}
-# Create a new journal entry
-function new_journal_entry() {
-  notes
-  echo '\n##'$(date +"%Y-%m-%d %H:%M")'\n' >> journal.md
-  vim -c 'startinsert' + journal.md
-  qnotes
-}
-alias nje=new_journal_entry
