f50488fa83ef39d788678fbbbac43f0814980198
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-03-11 22:34:30 -0700

    added lisp.vim and removed duplicate .vim

--- emacs/emacs.d.arch/custom.el
+++ emacs/emacs.d.arch/custom.el
@@ -3,7 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm-ebdb helm evil use-package))))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (nlinum-relative solarized-theme evil-escape helm-ebdb helm evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/emacs.d.arch/init.el
+++ emacs/emacs.d.arch/init.el
@@ -1,15 +1,11 @@
-(package-initialize)
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
+(package-initialize)
 (setq package-enable-at-startup nil)
-
-;; Don't litter my init file
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror)
 
 ;; Install use-package
 (unless (package-installed-p 'use-package)
@@ -19,8 +15,14 @@
 (eval-when-compile
   (require 'use-package))
 
+;; Basic Settings
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono Book 9"))
 
-;; Misc Settings
+;; Don't litter my init file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;; GUI Settings
 (setq inhibit-splash-screen t
       inhibit-startup-message t
       inhibit-startup-echo-area-message t)
@@ -35,18 +37,47 @@
 (setq-default indent-tabs-mode nil)
 
 
-
 ;; Use use-package to install other packages
 (use-package evil
   :ensure t
   :config
   (evil-mode 1)
+  (use-package evil-escape
+    :init
+    (setq evil-escape-key-sequence "jw")
+    :config
+    (define-key evil-normal-state-map "ZZ" 'evil-save-modified-and-close)
+    (define-key evil-normal-state-map "ZQ" 'evil-quit)
+    (evil-escape-mode 1))
   ;; More configuration goes here
   )
+
 
 (use-package helm
   :ensure t
   :config
   (helm-mode 1)
+  (define-key helm-map (kbd "C-j") 'helm-next-line)
+  (define-key helm-map (kbd "C-k") 'helm-previous-line)
   :bind (("M-x" . helm-M-x)
-	 ("C-x C-f" . helm-find-files)))
+         ("C-x C-f" . helm-find-files)))
+
+
+(use-package solarized-theme
+  :ensure t
+  :config
+  (load-theme 'solarized-dark))
+
+
+;;; If `display-line-numbers-mode' is available (only in Emacs 26),
+;;; use it! Otherwise, install and run nlinum-relative.
+(if (functionp 'display-line-numbers-mode)
+    (and (add-hook 'display-line-numbers-mode-hook
+                   (lambda () (setq display-line-numbers-type 'relative)))
+         (add-hook 'prog-mode-hook #'display-line-numbers-mode))
+  (use-package nlinum-relative
+    :ensure t
+    :config
+    (nlinum-relative-setup-evil)
+    (setq nlinum-relative-redisplay-delay 0)
+    (add-hook 'prog-mode-hook #'nlinum-relative-mode)))
