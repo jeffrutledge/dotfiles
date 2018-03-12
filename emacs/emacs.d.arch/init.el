98e51bf46ee97e8e80aa1b471813a0128f314e1a
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-03-11 17:33:51 -0700

    added emacs init.el

---
+++ emacs/emacs.d.arch/init.el
@@ -0,0 +1,29 @@
+(package-initialize)
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+
+;; Don't litter my init file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+
+;; Use use-package to install other packages
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+  ;; More configuration goes here
+  )
