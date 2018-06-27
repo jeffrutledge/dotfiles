57c0c25d1e3b2dd770e54ef6e0d5771bba956f48
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-06-26 21:39:32 -0500

    added rswifi

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -21,41 +21,42 @@ alias -s pdf='pdf-open'
 # Linux Exclusive aliases
 if [[ "$(uname -s)" == "Linux" ]]
 then
-  alias e='emacsclient -nc'
-  alias ls='ls --color=auto'
-  alias vim='vim --servername vim' # for vimtex latexmk callbacks
+    alias e='emacsclient -nc'
+    alias ls='ls --color=auto'
+    alias vim='vim --servername vim' # for vimtex latexmk callbacks
+    alias rswifi='connmanctl disable wifi && sleep 0.1 && connmanctl enable wifi'
 fi
 
 # Mac OSX Exclusive aliases
 if [[ "$(uname -s)" == "Darwin" ]]
 then
-  # MacVim
-  alias vim='mvim -v'
-  alias vi='mvim'
+    # MacVim
+    alias vim='mvim -v'
+    alias vi='mvim'
 
-  # Skim
-  alias skim='open -a Skim $1'
+    # Skim
+    alias skim='open -a Skim $1'
 
-  # mathmatica REPL
-  alias math='/Applications/Mathematica.app/Contents/MacOS/MathKernel'
+    # mathmatica REPL
+    alias math='/Applications/Mathematica.app/Contents/MacOS/MathKernel'
 
-  # Personal
-  # Open and close notes
-  function onotes() {
-    hdiutil attach ~/safe/personal/notes.dmg && cd /Volumes/notes
-  }
-  function qnotes() {
-    cd ~
-    local disk_name=$(hdiutil info | grep /Volumes/notes | cut -f1)
-    hdiutil detach $disk_name
-  }
-  # Create a new journal entry
-  function new_journal_entry() {
-    if onotes; then
-      echo '\n##'$(date +"%Y-%m-%d %H:%M")'\n' >> journal.md
-      vim -c 'startinsert' + journal.md
-      qnotes
-    fi
-  }
-  alias nje=new_journal_entry
+    # Personal
+    # Open and close notes
+    function onotes() {
+	hdiutil attach ~/safe/personal/notes.dmg && cd /Volumes/notes
+    }
+    function qnotes() {
+	cd ~
+	local disk_name=$(hdiutil info | grep /Volumes/notes | cut -f1)
+	hdiutil detach $disk_name
+    }
+    # Create a new journal entry
+    function new_journal_entry() {
+	if onotes; then
+	    echo '\n##'$(date +"%Y-%m-%d %H:%M")'\n' >> journal.md
+	    vim -c 'startinsert' + journal.md
+	    qnotes
+	fi
+    }
+    alias nje=new_journal_entry
 fi
