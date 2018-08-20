ca7845fa530e89a722dc785fa97544d661691903
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-08-20 13:49:06 -0700

    removed unused vim config, added emacs easymotion

--- emacs/emacs.d.arch/init_dir/evil.el
+++ emacs/emacs.d.arch/init_dir/evil.el
@@ -5,6 +5,7 @@
   :ensure t
   :after (evil-leader)
   :config
+  (define-key evil-motion-state-map (kbd "SPC") nil)
   (evil-mode 1))
 
 (use-package evil-leader
@@ -26,16 +27,24 @@
     "kpb" 'projectile-kill-buffers)
   (global-evil-leader-mode t))
 
-;; (use-package evil-org
-;; :ensure t
-;; :after org
-;; :config
-;; (add-hook 'org-mode-hook 'evil-org-mode)
-;; (add-hook 'evil-org-mode-hook
-;; (lambda ()
-;; (evil-org-set-key-theme)))
-;; (require 'evil-org-agenda)
-;; (evil-org-agenda-set-keys))
+(use-package evil-easymotion
+  :ensure t
+  :config
+  (evilem-make-motion
+   evilem-motion-forward-word-begin #'evil-forward-word-begin)
+  (evilem-define (kbd "SPC w") 'evilem-motion-forward-word-begin))
+  ;; (evilem-default-keybindings "SPC"))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+	    (lambda ()
+	      (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 (provide 'evil)
 ;;; evil.el ends here
