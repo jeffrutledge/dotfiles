028bfc18191c9b3605ad671be70ba06aed2e4c5c
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-08-21 10:03:26 -0700

    cleaned up config, added inputrc

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,61 +1,44 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
-export EDITOR='emacsclient -nc'
+export EDITOR='vim'
 
 # compile with multiple jobs
 export MAKEFLAGS='-j'
 
+# ls color for MAC OS X
 export CLICOLOR=true
-
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
 setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
 setopt NO_LIST_BEEP
+setopt CORRECT
+setopt IGNORE_EOF
+setopt PROMPT_SUBST
+
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
 
+setopt COMPLETE_IN_WORD
+setopt COMPLETE_ALIASES # don't expand aliases _before_ completion has finished
+
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions
+setopt EXTENDED_HISTORY # add timestamps to history
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
-
 bindkey -v
-bindkey -M viins 'jw' vi-cmd-mode
 
-# add some plugins
-if test $(which brew) &>/dev/null
-then
-  # add z plugin
-  source `brew --prefix`/etc/profile.d/z.sh
-
-  # add syntax highlighting
-  source `brew --prefix`/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-fi
+manual_package_paths=(
+  $HOME'/manual_packages/z/z.sh'
+  '/usr/share/fzf/completion.zsh'
+  '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+)
 if [[ "$(uname -s)" == "Linux" ]]
 then
-  source ~/manual_packages/z/z.sh
-  source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-  source /usr/share/fzf/completion.zsh
+  for p in $manual_package_paths
+  do
+    [[ -a $p ]] && source $p && echo sourced $p
+  done
 fi
