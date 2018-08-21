c21af7cad37559c8bcc05d9987d8f2364d05d45d
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-08-21 13:06:51 -0700

    fixed stow_group.sh to work before bin has been stowed

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Dotfiles
 
 Uses GNU `stow`.
+Directories starting with `_` are not meant to be stowed.
 
-## Setup
+## Usage
 Run `stow_group.sh [dotfile group]`.
 Dotfile groups can be found in `dotfile_groups`
+
+This sets up `stow` and stows `bin` so there is access to `stow_hook`.
+`stow_hook`: runs `pre_install.sh`, stows the directory, runs `post_install.sh`.
--- stow_group.sh
+++ stow_group.sh
@@ -1,17 +1,23 @@
 #!/bin/zsh
 
-# Setup stow and bin
-stow stow
-stow bin
+$stow_hook=./bin/bin/stow_hook
 
-for dotpackage in `cat $1`
-do
-  echo stowing $dotpackage
-  if stow_hook $dotpackage
-  then
-    echo finished stowing $dotpackage
-  else
-    echo failed to stow $dotpackage
-  fi
-  echo
-done
+echo setting up stow and bin
+if stow stow && stow bin
+then
+  echo finished setting up stow and bin
+
+  for dotpackage in `cat $1`
+  do
+    echo stowing $dotpackage
+    if $stow_hook $dotpackage
+    then
+      echo finished stowing $dotpackage
+    else
+      echo failed to stow $dotpackage
+    fi
+    echo
+  done
+else
+  echo failed to setup stow
+fi
