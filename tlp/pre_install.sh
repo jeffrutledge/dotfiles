fb7fc5f5fc54919eec0bb8a0ce6934bdadc87e68
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-08-26 19:59:20 -0700

    converted tlp to stow

--- tlp/pre_install.sh
+++ tlp/pre_install.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
 
+echo "Installing TLP"
+
+pacman -S tlp
+
+echo "Configuring TLP"
+
+# Mask to avoid conflicts
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket
+
+systemctl enable --now tlp.service
+systemctl enable --now tlp-sleep.service
+
+echo "\nRemember to set USB_BLACKLIST_PHONE=1 in /etc/default/tlp to enable phone charging.\n"
+
+echo "Finished Configuring TLP"
