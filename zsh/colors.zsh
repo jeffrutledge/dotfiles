3f74aa8cce32c156349f704b402c27c2ad891b0e
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-10-05 16:08:16 -0700

    added solarized color variables to zsh

---
+++ zsh/colors.zsh
@@ -0,0 +1,26 @@
+##### Solarized Colors
+# Solarized color scheme for the X Window System
+#
+# http://ethanschoonover.com/solarized
+
+# Common
+
+S_yellow=#b58900
+S_orange=#cb4b16
+S_red=#dc322f
+S_magenta=#d33682
+S_violet=#6c71c4
+S_blue=#268bd2
+S_cyan=#2aa198
+S_green=#859900
+
+# Dark
+
+S_base03=#002b36
+S_base02=#073642
+S_base01=#586e75
+S_base00=#657b83
+S_base0=#839496
+S_base1=#93a1a1
+S_base2=#eee8d5
+S_base3=#fdf6e3
