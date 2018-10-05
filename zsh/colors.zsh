e60bc89ca44434188f8252cd390559cc17ac1477
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2018-10-05 16:48:26 -0700

    corrected colors to be exported variables, added timewmenu script

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -5,22 +5,22 @@
 
 # Common
 
-S_yellow=#b58900
-S_orange=#cb4b16
-S_red=#dc322f
-S_magenta=#d33682
-S_violet=#6c71c4
-S_blue=#268bd2
-S_cyan=#2aa198
-S_green=#859900
+export S_yellow=#b58900
+export S_orange=#cb4b16
+export S_red=#dc322f
+export S_magenta=#d33682
+export S_violet=#6c71c4
+export S_blue=#268bd2
+export S_cyan=#2aa198
+export S_green=#859900
 
 # Dark
 
-S_base03=#002b36
-S_base02=#073642
-S_base01=#586e75
-S_base00=#657b83
-S_base0=#839496
-S_base1=#93a1a1
-S_base2=#eee8d5
-S_base3=#fdf6e3
+export S_base03=#002b36
+export S_base02=#073642
+export S_base01=#586e75
+export S_base00=#657b83
+export S_base0=#839496
+export S_base1=#93a1a1
+export S_base2=#eee8d5
+export S_base3=#fdf6e3
