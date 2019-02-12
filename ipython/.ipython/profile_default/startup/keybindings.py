cddd65d34bdf28864d8ddb803bd1b1a94f6be128
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2019-02-11 20:50:31 -0800

    removed jw as esc in ipy

--- ipython/.ipython/profile_default/startup/keybindings.py
+++ ipython/.ipython/profile_default/startup/keybindings.py
@@ -1,6 +1,4 @@
 from IPython import get_ipython
-from prompt_toolkit.enums import DEFAULT_BUFFER
-from prompt_toolkit.filters import HasFocus, ViInsertMode
 from prompt_toolkit.key_binding.vi_state import InputMode
 
 
@@ -10,10 +8,3 @@ ip = get_ipython()
 def switch_to_navigation_mode(event):
     vi_state = event.cli.vi_state
     vi_state.reset(InputMode.NAVIGATION)
-
-
-if getattr(ip, 'pt_cli'):
-    registry = ip.pt_cli.application.key_bindings_registry
-    registry.add_binding(u'j', u'w',
-                         filter=(HasFocus(DEFAULT_BUFFER)
-                                 & ViInsertMode()))(switch_to_navigation_mode)
