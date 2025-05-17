05e89d9f277dadfed83efe2b21ef3ded9bf1dc19
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2025-05-17 13:19:42 -0400

    added some binding overrides in norg

---
+++ neo_vim/.config/nvim/after/ftplugin/norg.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("i", "<C-CR>", "<Plug>(neorg.itero.next-iteration)", { buffer = true })
+vim.keymap.set("i", "<C-.>", "<Plug>(neorg.promo.promote)", { buffer = true })
+vim.keymap.set("i", "<C-,>", "<Plug>(neorg.promo.demote)", { buffer = true })
