dacbad533e6d2bd84d685ab4b3536707528522ca
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2025-12-31 10:44:08 -0800

    changed some org mappings

--- neo_vim/.config/nvim/lua/plugins/orgmode.lua
+++ neo_vim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,4 +1,17 @@
 require('orgmode').setup({
   org_agenda_files = '~/safe/orgfiles/**/*',
   org_default_notes_file = '~/safe/orgfiles/refile.org',
+  mappings = {
+      prefix = '<Leader><Leader>',
+  }
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'org',
+  callback = function()
+    vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+      silent = true,
+      buffer = true,
+    })
+  end,
 })
